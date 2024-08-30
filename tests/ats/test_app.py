28bd8149fa29c33eeee2b6175ec9d1d562caa43a
QuantumEnigmaa <thibaud@giantswarm.io>
2024-08-30 14:33:03 +0200

    more nitpicking

--- tests/ats/test_app.py
+++ tests/ats/test_app.py
@@ -11,8 +11,8 @@ from pytest_helm_charts.k8s.stateful_set import wait_for_stateful_sets_to_run
 logger = logging.getLogger(__name__)
 
 namespace_name = "loki"
-write_sts_name = "loki-write"
-backend_sts_name = "loki-backend"
+write_statefulset_name = "loki-write"
+backend_statefulset_name = "loki-backend"
 read_deployment_name = "loki-read"
 gateway_deployment_name = "loki-gateway"
 
@@ -33,16 +33,16 @@ def test_api_working(kube_cluster: Cluster) -> None:
 # scope "module" means this is run only once, for the first test case requesting! It might be tricky
 # if you want to assert this multiple times
 @pytest.fixture(scope="module")
-def ic_components(kube_cluster: Cluster) -> Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]:
+def components(kube_cluster: Cluster) -> Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]:
     logger.info("Waiting for loki components to be deployed..")
 
-    components_ready = wait_for_ic_components(kube_cluster)
+    components_ready = wait_for_components(kube_cluster)
 
     logger.info("loki components are deployed..")
 
     return components_ready
 
-def wait_for_ic_components(kube_cluster: Cluster) -> Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]:
+def wait_for_components(kube_cluster: Cluster) -> Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]:
     deployments = wait_for_deployments_to_run(
         kube_cluster.kube_client,
         [read_deployment_name, gateway_deployment_name],
@@ -51,7 +51,7 @@ def wait_for_ic_components(kube_cluster: Cluster) -> Tuple[List[pykube.Deploymen
     )
     statefulsets = wait_for_stateful_sets_to_run(
         kube_cluster.kube_client,
-        [write_sts_name, backend_sts_name],
+        [write_statefulset_name, backend_statefulset_name],
         namespace_name,
         timeout,
     )
@@ -72,13 +72,13 @@ def pods(kube_cluster: Cluster) -> List[pykube.Pod]:
 @pytest.mark.smoke
 @pytest.mark.upgrade
 @pytest.mark.flaky(reruns=5, reruns_delay=10)
-def test_pods_available(ic_components: Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]):
+def test_pods_available(components: Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]):
     # loop over the list of deployments
-    for d in ic_components[0]:
+    for d in components[0]:
         assert int(d.obj["status"]["readyReplicas"]) == int(d.obj["spec"]["replicas"])
 
     # loop over the list of statefulsets
-    for s in ic_components[1]:
+    for s in components[1]:
         assert int(s.obj["status"]["readyReplicas"]) == int(s.obj["spec"]["replicas"])
 
 # when we start the tests on circleci, we have to wait for pods to be available, hence
