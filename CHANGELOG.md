d6bf98e83337e17d56f68f6dae2709c5bd251025
Zirko <64951262+QuantumEnigmaa@users.noreply.github.com>
2024-09-05 09:39:24 +0200

    add automated tests for the chart (#343)
    * add automated tests for the chart
    
    * add ats to circleci config
    
    * persist chart archive for ats
    
    * move ats to python 3.9
    
    * fix imports
    
    * fix errors in testing code
    
    * add test values' content
    
    * update test values
    
    * update namespace in test file
    
    * fix test values
    
    * increase timeout
    
    * add chart templating test job
    
    * update ignore in circleci config
    
    * enable multi-tenant-proxy secret creation
    
    * add coredns netpol as extraObject in test values
    
    * add discovery port
    
    * update test-values and enable minio
    
    * more nitpicking
    
    * update test-values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add helm chart templating test in ci pipeline.
+- Add tests with ats in ci pipeline.
+
 ## [0.22.0] - 2024-08-12
 
 ### Changed
---
+++ tests/ats/test_app.py
@@ -0,0 +1,85 @@
+import logging
+from typing import List, Tuple
+
+import pytest
+import pykube
+from pytest_helm_charts.clusters import Cluster
+from pytest_helm_charts.k8s.deployment import wait_for_deployments_to_run
+from pytest_helm_charts.k8s.stateful_set import wait_for_stateful_sets_to_run
+
+
+logger = logging.getLogger(__name__)
+
+namespace_name = "loki"
+write_statefulset_name = "loki-write"
+backend_statefulset_name = "loki-backend"
+read_deployment_name = "loki-read"
+gateway_deployment_name = "loki-gateway"
+
+timeout: int = 900
+
+@pytest.mark.smoke
+def test_api_working(kube_cluster: Cluster) -> None:
+    """Very minimalistic example of using the [kube_cluster](pytest_helm_charts.fixtures.kube_cluster)
+    fixture to get an instance of [Cluster](pytest_helm_charts.clusters.Cluster) under test
+    and access its [kube_client](pytest_helm_charts.clusters.Cluster.kube_client) property
+    to get access to Kubernetes API of cluster under test.
+    Please refer to [pykube](https://pykube.readthedocs.io/en/latest/api/pykube.html) to get docs
+    for [HTTPClient](https://pykube.readthedocs.io/en/latest/api/pykube.html#pykube.http.HTTPClient).
+    """
+    assert kube_cluster.kube_client is not None
+    assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
+
+# scope "module" means this is run only once, for the first test case requesting! It might be tricky
+# if you want to assert this multiple times
+@pytest.fixture(scope="module")
+def components(kube_cluster: Cluster) -> Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]:
+    logger.info("Waiting for loki components to be deployed..")
+
+    components_ready = wait_for_components(kube_cluster)
+
+    logger.info("loki components are deployed..")
+
+    return components_ready
+
+def wait_for_components(kube_cluster: Cluster) -> Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]:
+    deployments = wait_for_deployments_to_run(
+        kube_cluster.kube_client,
+        [read_deployment_name, gateway_deployment_name],
+        namespace_name,
+        timeout,
+    )
+    statefulsets = wait_for_stateful_sets_to_run(
+        kube_cluster.kube_client,
+        [write_statefulset_name, backend_statefulset_name],
+        namespace_name,
+        timeout,
+    )
+    return (deployments, statefulsets)
+
+@pytest.fixture(scope="module")
+def pods(kube_cluster: Cluster) -> List[pykube.Pod]:
+    pods = pykube.Pod.objects(kube_cluster.kube_client)
+
+    pods = pods.filter(namespace=namespace_name, selector={
+                       'app.kubernetes.io/name': 'loki', 'app.kubernetes.io/instance': 'loki'})
+
+    return pods
+
+# when we start the tests on circleci, we have to wait for pods to be available, hence
+# this additional delay and retries
+
+@pytest.mark.smoke
+@pytest.mark.upgrade
+@pytest.mark.flaky(reruns=5, reruns_delay=10)
+def test_pods_available(components: Tuple[List[pykube.Deployment], List[pykube.StatefulSet]]):
+    # loop over the list of deployments
+    for d in components[0]:
+        assert int(d.obj["status"]["readyReplicas"]) == int(d.obj["spec"]["replicas"])
+
+    # loop over the list of statefulsets
+    for s in components[1]:
+        assert int(s.obj["status"]["readyReplicas"]) == int(s.obj["spec"]["replicas"])
+
+# when we start the tests on circleci, we have to wait for pods to be available, hence
+# this additional delay and retries
