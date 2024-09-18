9925fa68fdd3a1222f2c82e93e783f01ad77b101
QuantumEnigmaa <thibaud@giantswarm.io>
2024-09-18 12:03:40 +0200

    refacto code

--- tests/manual_e2e/basic_checks.sh
+++ tests/manual_e2e/basic_checks.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
+# Helper function - prints an error message and exits
+exit_error() {
+    echo "Error: $*"
+    exit 1
+}
+
 echo "Checking if loki app is in deployed state"
 
 deployed=$(kubectl get app -n giantswarm loki -o yaml | yq .status.release.status)
 
-if [[ "$deployed" != "deployed" ]]; then
-  echo "loki app is not in deployed state. Please fix the app before retrying"
-  exit 1
-else
-  echo "loki app is indeed in deployed state"
-fi
+[[ "$deployed" != "deployed" ]] && exit_error "loki app is not in deployed state. Please fix the app before retrying" || echo "loki app is indeed in deployed state"
 
 echo "Checking if loki-canary is enabled"
 
 canary=$(kubectl get cm -n giantswarm loki-chart-values -oyaml | yq .data.values | yq .loki.lokiCanary.enabled)
 
-if [[ "$canary" != "true" ]]; then
-  echo "loki-canary is not enabled. Please enable it before retrying"
-  exit 1
-else
-  echo "loki-canary is indeed enabled"
-fi
+[[ "$canary" != "true" ]] && exit_error "loki-canary is not enabled. Please enable it before retrying" || echo "loki-canary is indeed enabled"
 
 echo "Checking if all loki pods are up and running"
 
-lokiComponents=("read" "write" "backend" "gateway")
+lokiComponents=("read" "write" "backend" "gateway" "canary")
 
 for component in "${lokiComponents[@]}"; do
   podStatus=$(kubectl get pods -n loki -l app.kubernetes.io/name=loki,app.kubernetes.io/component=$component -o yaml | yq .items[].status.phase)
 
+  [[ -z "$podStatus" ]] && exit_error "No $component pods found. Please check it before retrying"
+
   for status in $podStatus; do
-    if [[ "$status" != "Running" ]]; then
-      echo "A $component pod is not running. Please check it before retrying"
-      exit 1
-    fi
+    [[ "$status" != "Running" ]] && exit_error "A $component pod is not running. Please check it before retrying"
   done
 done
 
